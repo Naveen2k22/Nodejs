22b3c57fbbbbb019de80e7fc6925444907fbdfee
Naveen2k22 <99787149+Naveen2k22@users.noreply.github.com>
2022-03-10 12:38:54 +0530

    changed a console.log

--- Naveen/server.js
+++ Naveen/server.js
@@ -23,7 +23,7 @@ app.listen(3000, function(){
 })
 app.get('/hostel',async (req,res) =>
 {
-    console.log('hello world')
+    console.log('hello!')
     const hostellist = await hostel.find()
     console.log(hostellist)
     res.json(hostellist);
